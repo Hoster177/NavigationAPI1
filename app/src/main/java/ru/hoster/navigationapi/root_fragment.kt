e80e99adf0824b117ae667bcf02c71e3de3bc1b0
Hoster177 <hoster177@gmail.com>
2024-12-19 15:19:24 +0300

    Initial commit

---
+++ app/src/main/java/ru/hoster/navigationapi/root_fragment.kt
@@ -0,0 +1,66 @@
+package ru.hoster.navigationapi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import ru.hoster.navigationapi.databinding.FragmentBlankBinding
+import ru.hoster.navigationapi.databinding.FragmentRootFragmentBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [root_fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class root_fragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentRootFragmentBinding= DataBindingUtil.inflate(inflater,R.layout.fragment_root_fragment,container,false)
+        binding.ButtonRoot.setOnClickListener {
+            binding.root.findNavController().navigate(R.id.action_root_fragment_to_blankFragment)
+        }
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment root_fragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            root_fragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
